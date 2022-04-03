e694bcebf9f8f67d56e5c094f2788575dd852460
LuCC-que <chen1968493689@users.github.com>
2022-04-03 17:38:55 -0400

    recipes updated

--- src/components/active/activeComponent/ContainerComponent/FoodrecExpand.js
+++ src/components/active/activeComponent/ContainerComponent/FoodrecExpand.js
@@ -1,4 +1,3 @@
-import "./TempertureExpand.css";
 import "./FoodRec.css";
 
 const FoodrecExpand = (props) => {
@@ -23,12 +22,18 @@ const FoodrecExpand = (props) => {
       <div className="otherRecipe1"></div>
       <div className="otherRecipe2"></div>
       <div className="salmon"></div>
+      <div className="recipeFish"></div>
+      <div className="recipeFruit"></div>
+      <div className="recipePepper"></div>
+      <div className="recipeFishCopy"></div>
+      <div className="recipeFruitCopy"></div>
+      <div className="recipePepperCopy"></div>
 
       <div className="step1">
         <strong>
-          Bake 250g salmon in oven for
+          Bake 250g salmon in oven for 10 to 20
           <br />
-          10 to 20 minutes at 400 F
+          minutes at 400 F
         </strong>
       </div>
       <div className="step2">
